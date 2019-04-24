11b9dad856f268e942d4d0bd791f8ded8dcd1fa8
CxpKing <602869572@qq.com>
2019-04-24 22:23:21 +0800

    Add some login fun doc

--- webblog/account/views.py
+++ webblog/account/views.py
@@ -4,7 +4,14 @@ from django.contrib.auth import authenticate,login
 from .forms import LoginForm
 # Create your views here.
 
+
 def user_login(request):
+	'''
+	login function
+	login_form.is_valid 在创建实例时，如果传递给表单的数据是符合表单类属性要求的，则返回True，否则返回False
+	login_form.cleaned_data 以字典的形式返回实例的具体数据。如果传入的某项数据不合法，则在cleaned_data的结果中不予显示。
+
+	'''
 	if request.method == "POST":
 		login_form = LoginForm(request.POST)
 		if login_form.is_valid():
