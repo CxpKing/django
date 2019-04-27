cdcaa7612cde24cda62edfc1badf14cdd98c599a
CxpKing <602869572@qq.com>
2019-04-27 22:18:28 +0800

    Add change password option

--- webblog/account/urls.py
+++ webblog/account/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
 	#系统内置的退出
 	path(r'loginout/',auth_views.LogoutView.as_view(template_name="account/logout.html"),name='user_logout'),
 	path(r'register/',views.user_register,name='user_register'),
+	path('password-change/', auth_views.PasswordChangeView.as_view(template_name="account/password_change_form.html", success_url="/account/password-change-done/"), name='password_change'),
+	path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name="account/password_change_done.html"), name='password_change_done'),
 ]
